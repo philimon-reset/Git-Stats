732a961cf86297ab8b07af7c722d79394ba3e993
Hizkyas Tekle <hizkyastekle@gmail.com>
2021-10-20 16:45:28 +0300

    starting point

---
+++ wrapper/user_wrapper.py
@@ -0,0 +1,19 @@
+from requests import get, post
+
+
+def get_user(token, etag):
+    """
+        Gets public user information.
+        returns None if user information hasn't changed based on the etag
+        returns a dict if user information has been updated
+    """
+    token = "token "+ token
+    headers = {
+        "Authorization": token,
+        "If-None-Match": etag
+    }
+    result = get("https://api.github.com/user", headers=headers)
+    if result.status_code == 304:
+        return None
+    else:
+        return result
